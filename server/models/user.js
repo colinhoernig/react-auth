74f8bd680aeea8bed2b291fcb05baf9d76b311f1
Colin Hoernig <Hoernig.Colin@gmail.com>
2016-03-20 16:42:47 -0500

    Implement password hashing in User model with bcrypt.

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // Define the user model
 const userSchema = new Schema({
@@ -9,6 +10,26 @@ const userSchema = new Schema({
     lowercase: true
   },
   password: String
+});
+
+// Encrypt password on save hook
+userSchema.pre('save', function(next) {
+  // Get access to the user model context
+  const user = this;
+
+  // Generate a salt, then run callback
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+
+    // Encrypt our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+
+      // Overwrite plain-text password with encrypted password
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 // Create the model class
